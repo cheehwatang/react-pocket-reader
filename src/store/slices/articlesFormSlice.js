8005deba583efa487541cd2db0eb45d0e56fad8f
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-04-14 13:35:56 +0800

    Adds reducers for changeName, changeContent and changeAuthor

--- src/store/slices/articlesFormSlice.js
+++ src/store/slices/articlesFormSlice.js
@@ -8,7 +8,19 @@ const articlesFormSlice = createSlice({
     author: "",
     dateAdded: "",
   },
-  reducers: {},
+  reducers: {
+    changeName(state, action) {
+      state.title = action.payload;
+    },
+    changeContent(state, action) {
+      state.content = action.payload;
+    },
+    changeAuthor(state, action) {
+      state.author = action.payload;
+    },
+  },
 });
 
+export const { changeName, changeContent, changeAuthor } =
+  articlesFormSlice.actions;
 export const articlesFormReducer = articlesFormSlice.reducer;
